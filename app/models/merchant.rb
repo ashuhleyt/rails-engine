576df76e5445e90afe1e06aea45f5c1952b373d9
Ashley <ashuhleyt@gmail.com>
2022-12-06 11:39:43 -0800

    completed merchant show

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,4 @@
 class Merchant < ApplicationRecord
+  
 
-
-end
+end 
--- spec/requests/merchant_request_spec.rb
+++ spec/requests/merchant_request_spec.rb
@@ -22,5 +22,19 @@ describe "merchants API" do
         expect(merchant[:attributes][:name]).to be_a(String)
       end
     end
+
+    it 'can return a single merchant' do 
+      id = create(:merchant).id 
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful 
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_a(Integer)
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
   end
 end
