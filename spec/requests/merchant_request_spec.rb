23c58893edce7a317c42d617030d529304cda303
Ashley <ashuhleyt@gmail.com>
2022-12-06 11:09:03 -0800

    finished returning all merchants

---
+++ spec/requests/merchant_request_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "merchants API" do 
+  context "GET /api/v1/merchants" do 
+    it 'sends a list of merchants' do 
+      create_list(:merchant, 3)
+
+      get "/api/v1/merchants"
+
+      expect(response).to be_successful 
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants[:data].count).to eq(3)
+
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(String) 
+        expect(merchant).to have_key(:type)
+        expect(merchant[:type]).to be_a(String)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+  end
+end
