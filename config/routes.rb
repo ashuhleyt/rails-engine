de8ccd3973875b32611fa00ba15e0c6665eb4bd8
Ashley <ashuhleyt@gmail.com>
2022-12-06 13:25:39 -0800

    finished items show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do 
         resources :items, only: [:index]
       end
+      resources :items, only: [:index]
     end
   end
 end
--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :item do
+    merchant 
     name { Faker::Music::Hiphop.artist }
     description { Faker::Movies::HarryPotter.quote }
     unit_price { Faker::Number.decimal(l_digits: 2) }
