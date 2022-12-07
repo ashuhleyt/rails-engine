ac3adb25932a86590af773c9f617bdcd1cfb270e
Ashley <ashuhleyt@gmail.com>
2022-12-07 13:41:29 -0800

    finished testing for items belonging to merchant

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Invoice, type: :model do
   describe 'relationships' do 
     it { should belong_to(:customer) }
     it { should have_many(:invoice_items) }
-    it { should have(:items).through(:invoice_items) }
+    it { should have_many(:items).through(:invoice_items) }
   end
 end
